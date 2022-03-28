0c0b89af945ac4726e18694eedea59677775552d
Stephane Delcroix <stephane@delcroix.org>
2022-03-28 15:21:01 +0200

    [X] Optimiz OnPlatform element syntax
    - fixes #5583

--- src/Controls/tests/Xaml.UnitTests/OnPlatformOptimization.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/OnPlatformOptimization.xaml.cs
@@ -10,7 +10,6 @@ using Mono.Cecil;
 
 namespace Microsoft.Maui.Controls.Xaml.UnitTests
 {
-	[XamlCompilation(XamlCompilationOptions.Skip)]
 	public partial class OnPlatformOptimization : ContentPage
 	{
 		public OnPlatformOptimization()
@@ -33,15 +32,42 @@ namespace Microsoft.Maui.Controls.Xaml.UnitTests
 				Assert.That(!methodDef.Body.Instructions.Any(instr=>InstructionIsOnPlatformExtensionCtor(methodDef, instr)), "This Xaml still generates a new OnPlatformExtension()");
 			}
 
+			[Test]
+			public void OnPlatformAreSimplified([Values("net6.0-ios", "net6.0-android")] string targetFramework)
+			{
+				MockCompiler.Compile(typeof(OnPlatformOptimization), out var methodDef, targetFramework);
+				Assert.That(!methodDef.Body.Instructions.Any(instr => InstructionIsOnPlatformCtor(methodDef, instr)), "This Xaml still generates a new OnPlatform()");
+				Assert.That(methodDef.Body.Instructions.Any(instr => InstructionIsLdstr(methodDef.Module, instr, "Mobile, eventually ?")), $"This Xaml doesn't generate content for {targetFramework}");
+				Assert.That(!methodDef.Body.Instructions.Any(instr => InstructionIsLdstr(methodDef.Module, instr, "Desktop, maybe ?")), $"This Xaml generates content not required for {targetFramework}");
+			}
+
+			bool InstructionIsOnPlatformCtor(MethodDefinition methodDef, Mono.Cecil.Cil.Instruction instruction)
+			{
+				if (instruction.OpCode != OpCodes.Newobj)
+					return false;
+				if (instruction.Operand is not MethodReference methodRef)
+					return false;
+				if (!Build.Tasks.TypeRefComparer.Default.Equals(methodRef.DeclaringType, methodDef.Module.ImportReference(typeof(OnPlatform<View>))))
+					return false;
+				return true;
+			}
+
 			bool InstructionIsOnPlatformExtensionCtor(MethodDefinition methodDef, Mono.Cecil.Cil.Instruction instruction)
 			{
 				if (instruction.OpCode != OpCodes.Newobj)
 					return false;
-				if (!(instruction.Operand is MethodReference methodRef))
+				if (instruction.Operand is not MethodReference methodRef)
 					return false;
 				if (!Build.Tasks.TypeRefComparer.Default.Equals(methodRef.DeclaringType, methodDef.Module.ImportReference(typeof(OnPlatformExtension))))
 					return false;
 				return true;
+			}
+
+			bool InstructionIsLdstr(ModuleDefinition module, Mono.Cecil.Cil.Instruction instruction, string str)
+			{
+				if (instruction.OpCode != OpCodes.Ldstr)
+					return false;
+				return instruction.Operand as string == str;
 			}
 		}
 	}
